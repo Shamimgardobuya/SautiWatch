057b74c6fe38288624550cb277af9b8d7dff0447
Akelvino <vinho892@gmail.com>
2025-10-08 12:35:25 +0300

    Linked apps.support tothe main project

--- apps/support/views.py
+++ apps/support/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import SupportContact
 
 # Create your views here.
+def support_contact(request):
+    supportcontact = SupportContact.objects.filter(is_verified=True)
+    return render(request, 'support/support_contact.html', {"supportcontact":supportcontact})
