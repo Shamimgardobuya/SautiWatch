874af06eb82964b1246f029aadca90a03243f370
Akelvino <vinho892@gmail.com>
2025-10-10 23:02:45 +0300

    Added the search feature

--- apps/support/views.py
+++ apps/support/views.py
@@ -3,5 +3,21 @@ from .models import SupportContact
 
 # Create your views here.
 def support_contact(request):
+    region = request.GET.get('region','')
+    category = request.GET.get('category','')
     supportcontact = SupportContact.objects.filter(is_verified=True)
-    return render(request, 'support/support_contact.html', {"supportcontact":supportcontact})
+    
+    if region:
+         supportcontact = supportcontact.filter(region__icontains=region)
+    
+    if category:
+        supportcontact = supportcontact.filter(category=category)
+    
+    context ={
+        "supportcontact":supportcontact,
+        "region":region,
+        "category":category,
+        
+    }
+    
+    return render(request, 'support/support_contact.html', context)
