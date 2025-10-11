0ee717fc8fb395b1b68160b0387c0b7b09dc24cb
Shamimgardobuya <shamimobuya@gmail.com>
2025-10-11 11:41:06 +0300

    added postgres for deployment

--- sauti_watch/settings.py
+++ sauti_watch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url 
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -109,10 +110,11 @@ WSGI_APPLICATION = 'sauti_watch.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 
