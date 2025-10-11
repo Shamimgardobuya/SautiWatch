8ee571eb05d627625c6c4379a583f0868598648b
Shamimgardobuya <shamimobuya@gmail.com>
2025-10-11 16:04:40 +0300

    removed custom user model

--- sauti_watch/settings.py
+++ sauti_watch/settings.py
@@ -18,7 +18,6 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-AUTH_USER_MODEL = 'users.CustomUser'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -35,7 +34,7 @@ CELERY_RESULT_BACKEND = "redis://localhost:6379"
 ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY").encode()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
